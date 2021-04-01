814df40a20d5ae614205142085652ffee1036b8a
sunist <sunist.c@outlook.com>
2021-04-02 07:58:00 +0800

    UPDATE WEEK-2

---
+++ src/algorithm/bfs.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: Sunist Chan
+ * @Date: 2021-04-02 07:54:50
+ * @LastEditTime: 2021-04-02 07:57:31
+ * @LastEditors: Sunist Chan
+ * @Description: 
+ * @FilePath: /2021-LQC-Trainning/src/algorithm/bfs.cpp
+ * <!-- i wanna play arknights -->
+ */
+#include <iostream>
+
+void dfs()
+{
+    return;
+}
